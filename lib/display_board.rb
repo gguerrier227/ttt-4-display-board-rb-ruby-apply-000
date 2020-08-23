f6778797592538ccce63818324dbc474daf8474f
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 14:41:18 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -12,30 +12,7 @@ def display_board(new)
 end
 
 board = [" "," "," "," "," "," "," "," "," "," "," "," "]
-puts display_board(board)
-
-#X center
-
-def display_board(new0)
-  puts "   |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
 board0 = [" "," "," "," "," ","X"," "," "," "," "," "," "]
+puts display_board(board)
 puts display_board(board0)
 
-#X center O top left 
-
-def display_board(new1)
-  puts " O |   |   "
-  puts "-----------"
-  puts "   | X |   "
-  puts "-----------"
-  puts "   |   |   "
-end
-
-board1 = ["O"," "," "," "," ","X"," "," "," "," "," "," "]
-puts display_board(board1)
