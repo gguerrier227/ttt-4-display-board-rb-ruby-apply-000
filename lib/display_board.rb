03c8755c41856e3b2b6e15e139c007ba7d939bdd
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 13:54:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display board
+board = [" "," "," "," "," "," "," "," "," "," "," "," "]
+def display board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
+display_board(board)
