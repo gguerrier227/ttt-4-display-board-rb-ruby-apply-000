ee63a01ba6d2d44069769eef8280e3715a956959
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 14:37:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,6 @@
 
 #Empty Board
 
-
 def display_board(new)
   puts "   |   |   "
   puts "-----------"
@@ -15,7 +14,22 @@ end
 board = [" "," "," "," "," "," "," "," "," "," "," "," "]
 print display_board(board)
 
+#X center
+
 def display_board(new)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+board = [" "," "," "," "," ","X"," "," "," "," "," "," "]
+print display_board(board)
+
+#X center O top left 
+
+def display_board(new1)
   puts " O |   |   "
   puts "-----------"
   puts "   | X |   "
@@ -23,5 +37,5 @@ def display_board(new)
   puts "   |   |   "
 end
 
-board = ["O"," "," "," "," ","X"," "," "," "," "," "," "]
-print display_board(board)
+board1 = ["O"," "," "," "," ","X"," "," "," "," "," "," "]
+print display_board(board1)
