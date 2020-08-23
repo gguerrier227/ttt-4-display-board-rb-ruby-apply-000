ff622d33165342f65bea6cfd4d26b20de534796e
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-08-23 15:05:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,25 +3,7 @@
 
 #Empty Board
 
-def display_board(board)
-   puts row0 = ["#{tl}" "|" "#{tm}" "|" "#{tr}"]
+def display_board
+   puts row = ["   " "|" "   " "|" "   "]
    puts separator = "-----------"
-   puts row1 = ["#{cl}" "|" "#{cm}" "|" "#{cr}"]
-   puts separator1 = "-----------"
-   puts row2 = ["#{bl}" "|" "#{bm}" "|" "#{br}"]
- end
-
-empty = [" "," "," "," "," "," "," "," "," "," "," "," "]
-puts display_board(empty)
-
-def display_board(board)
-   puts row0 = ["#{tl}" "|" "#{tm}" "|" "#{tr}"]
-   puts separator = "-----------"
-   puts row1 = ["#{cl}" "|" "#{cm}" "|" "#{cr}"]
-   puts separator1 = "-----------"
-   puts row2 = ["#{bl}" "|" "#{bm}" "|" "#{br}"]
- end
-
-cm = ['X']
-puts display_board(board0)
-
+   puts row
